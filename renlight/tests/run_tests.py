e2d1763e7261d2048c6faa28bec265e865ea01b1
Mario Vidov <mvidov@yahoo.com>
2013-10-14 22:38:17 +0200

    Add shapes directory to automatic tests

--- renlight/tests/run_tests.py
+++ renlight/tests/run_tests.py
@@ -3,4 +3,5 @@ import unittest
 from unittest import TestLoader
 suite = TestLoader().discover('base')
 suite.addTests(TestLoader().discover('sdl'))
+suite.addTests(TestLoader().discover('shapes'))
 unittest.TextTestRunner(verbosity=5).run(suite)
